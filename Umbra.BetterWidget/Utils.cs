56c131321ed6adc080be3650e779a114c603aecc
Valgrifer <valgrifer123@gmail.com>
2025-10-23 03:14:50 +0200

    Remove duplicate function

--- Umbra.BetterWidget/Utils.cs
+++ Umbra.BetterWidget/Utils.cs
@@ -6,8 +6,8 @@ namespace Umbra.BetterWidget;
 
 internal sealed class Utils
 {
-    [ConfigVariable("BetterTeleport.Favorites")]
-    private static string FavoritesData { get; set; } = "";
+    [ConfigVariable("Toolbar.IsTopAligned", "General", "Toolbar")]
+    public static bool IsTopAligned { get; set; } = false;
     
     internal static UdtDocument DocumentFrom(string resourceName)
     {
@@ -22,34 +22,5 @@ internal sealed class Utils
         }
         
         throw new Exception($"No UDT document with the name \"{resourceName}\" exists in any assembly.");
-    }
-
-    internal static string Encode(string text)
-    {
-        byte[]    bytes  = Encoding.UTF8.GetBytes(text);
-        using var output = new MemoryStream();
-
-        using (var deflateStream = new DeflateStream(output, CompressionLevel.SmallestSize)) {
-            deflateStream.Write(bytes, 0, bytes.Length);
-        }
-
-        bytes = output.ToArray();
-
-        return Convert.ToBase64String(bytes);
-    }
-
-    internal static string Decode(string text, string defaultValue = "{}")
-    {
-        if (string.IsNullOrEmpty(text)) return defaultValue;
-
-        byte[]    bytes  = Convert.FromBase64String(text);
-        using var input  = new MemoryStream(bytes);
-        using var output = new MemoryStream();
-
-        using (var deflateStream = new DeflateStream(input, CompressionMode.Decompress)) {
-            deflateStream.CopyTo(output);
-        }
-
-        return Encoding.UTF8.GetString(output.ToArray());
     }
 }
