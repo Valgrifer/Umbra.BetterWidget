2f97950b65336f5f3dd3afce32cf45a326f7cbc4
Valgrifer <valgrifer123@gmail.com>
2025-10-23 05:54:26 +0200

    Remove ConfigVariable for Reflection function
    cause ConfigVariable don't work in plugin and Toolbar is internal :/

--- Umbra.BetterWidget/Utils.cs
+++ Umbra.BetterWidget/Utils.cs
@@ -1,13 +1,11 @@
-using System.IO;
-using System.IO.Compression;
-using System.Text;
+using System.Reflection;
 
 namespace Umbra.BetterWidget;
 
 internal sealed class Utils
 {
-    [ConfigVariable("Toolbar.IsTopAligned", "General", "Toolbar")]
-    public static bool IsTopAligned { get; set; } = false;
+    public static bool Enabled      => GetToolbarConfigFieldValue<bool>("Enabled");
+    public static bool IsTopAligned => GetToolbarConfigFieldValue<bool>("IsTopAligned");
     
     internal static UdtDocument DocumentFrom(string resourceName)
     {
@@ -22,5 +20,43 @@ internal sealed class Utils
         }
         
         throw new Exception($"No UDT document with the name \"{resourceName}\" exists in any assembly.");
+    }
+    
+    /// <summary>
+    /// Retrieves the value of a public static field from the Toolbar class by its name, as a generic type.
+    /// </summary>
+    /// <typeparam name="T">The expected type of the field value.</typeparam>
+    /// <param name="fieldName">The name of the static field to retrieve.</param>
+    /// <returns>The value of the field cast to the specified generic type.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when the provided field name is null or empty.</exception>
+    /// <exception cref="TypeLoadException">Thrown when the Toolbar type cannot be loaded.</exception>
+    /// <exception cref="MissingFieldException">Thrown when the field does not exist or is not public static.</exception>
+    /// <exception cref="InvalidCastException">Thrown when the field value cannot be cast to the specified type.</exception>
+    private static T GetToolbarConfigFieldValue<T>(string fieldName)
+    {
+        if (string.IsNullOrWhiteSpace(fieldName))
+            throw new ArgumentNullException(nameof(fieldName), "Field name cannot be null or empty.");
+
+        Assembly asm = Framework.Assemblies
+                .FirstOrDefault(a => a.GetType("Umbra.Toolbar", false) != null)
+            ?? throw new InvalidOperationException("Assembly containing type Umbra.Toolbar not found.");
+
+        Type toolbarConfigType = asm.GetType("Umbra.Toolbar", throwOnError: false)
+            ?? throw new InvalidOperationException("Type Umbra.Toolbar not found.");
+
+        if (toolbarConfigType == null)
+            throw new TypeLoadException("Failed to load type 'Umbra.Toolbar' from assembly 'Umbra'.");
+
+        var field = toolbarConfigType.GetProperty(fieldName, BindingFlags.Public | BindingFlags.Static);
+
+        if (field == null)
+            throw new MissingFieldException($"Public static field '{fieldName}' was not found in Toolbar.");
+ 
+        var value = field.GetValue(null);
+
+        if (value is not T typedValue)
+            throw new InvalidCastException($"Field '{fieldName}' cannot be cast to type {typeof(T).FullName}.");
+
+        return typedValue;
     }
 }
