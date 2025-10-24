df453c929543c7ef8d2661b123fc38f490ef9bbf
Valgrifer <valgrifer123@gmail.com>
2025-10-24 11:58:23 +0200

    Use custom udt document

--- Umbra.BetterWidget/Widgets/DtrFilteredPopup/DtrPopupFilteredWidgetPopup.cs
+++ Umbra.BetterWidget/Widgets/DtrFilteredPopup/DtrPopupFilteredWidgetPopup.cs
@@ -8,8 +8,7 @@ public class DtrPopupFilteredWidgetPopup : WidgetPopup
     private const string ClassNameDecorated = "decorated";
     private DtrPopupFilteredWidget Widget { get; }
 
-    private UdtDocument Document { get; } = Utils.DocumentFrom("umbra.widgets._popup_menu.xml");
-    protected override Node Node { get; }
+    protected override Node Node { get; } = Utils.DocumentFrom("umbra.better_widgets._popup_menu.xml").RootNode!;
 
     private readonly IDtrBarEntryRepository _repository = Framework.Service<IDtrBarEntryRepository>();
 
@@ -20,44 +19,6 @@ public class DtrPopupFilteredWidgetPopup : WidgetPopup
     internal DtrPopupFilteredWidgetPopup(DtrPopupFilteredWidget widget)
     {
         Widget = widget;
-        Node = Document.RootNode!;
-        Document.Stylesheet!.AddRule(".popup", new()
-        {
-            Padding = new (8, 8, 3, 8),
-        });
-        Document.Stylesheet!.AddRule("."+ClassNameDtrEntry, new()
-        {
-            AutoSize = new(AutoSize.Grow, AutoSize.Fit),
-            Padding = new (5, 13),
-            BackgroundColor = new(0x00000000),
-            BorderRadius    = 5,
-            StrokeColor     = new(0x00000000),
-            StrokeWidth     = 1,
-            StrokeInset     = 1,
-            StrokeRadius    = 3,
-        });
-        Document.Stylesheet!.AddRule($".{ClassNameDtrEntry}>#Label", new()
-        {
-            Anchor       = Anchor.MiddleCenter,
-            Color        = new(0xFFB6B6B6),
-            OutlineColor = new(0xC0000000),
-            OutlineSize  = 1,
-            FontSize     = 13
-        });
-        Document.Stylesheet!.AddRule($".{ClassNameDtrEntry}.{ClassNameDecorated}>#Label", new()
-        {
-            Padding = new (0, 4),
-        });
-        Document.Stylesheet!.AddRule($".{ClassNameDtrEntry}.{ClassNameDecorated}", new()
-        {
-            BackgroundColor = new(0xFF040404),
-            StrokeColor     = new(0xFF343434), 
-        });
-        Document.Stylesheet!.AddRule($".{ClassNameDtrEntry}.{ClassNameDecorated}:hover", new()
-        {
-            BackgroundColor = new(0xFF202020),
-            StrokeColor     = new(0xFF444444), 
-        });
         
         _repository.OnEntryAdded   += OnDtrBarEntryAdded;
         _repository.OnEntryRemoved += OnDtrBarEntryRemoved;
@@ -88,6 +49,10 @@ public class DtrPopupFilteredWidgetPopup : WidgetPopup
         var textOffset   = Widget.GetConfigValue<int>("TextYOffset");
 
         Node.Style.Gap  = Widget.GetConfigValue<int>("ItemSpacing");
+        if (MaxTextWidth != null)
+            Node.Style.Size = new((float) (MaxTextWidth! + 26f), 0);
+        else
+            Node.Style.Size = null;
 
         foreach ((string id, Node node) in _entries) {
             switch (decorateMode) {
